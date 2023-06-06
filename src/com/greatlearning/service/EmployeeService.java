4241423961fdb525a45a63ae7e151307ab7a1b38
Suresh Palaparthi <91492334+Suresh-Palaparthi@users.noreply.github.com>
2023-06-06 11:41:57 +0530

    Formatted EmployeeService.java

--- src/com/greatlearning/service/EmployeeService.java
+++ src/com/greatlearning/service/EmployeeService.java
@@ -5,6 +5,8 @@ import com.greatlearning.model.Employee;
 public interface EmployeeService {
 
 	String generateUserEmail(String firstName, String lastName, String departmentName);
+	
 	String generateUserPassword();
+	
 	void displayEmployeeDetails(Employee e1);
 }
